f1d38d4a296870593fa21614bcb74bd546d7b490
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-08-05 20:39:52 +0600

    again from the start

--- lesson-23/Logo_DSfirst/js/main.js
+++
@@ -1,7 +0,0 @@
-const menuToggler = document.querySelector(".header__button");
-
-const menu = document.querySelector(".header__nav");
-
-menuToggler.addEventListener("click", function () {
-  menu.classList.toggle("js-menu-show");
-});
