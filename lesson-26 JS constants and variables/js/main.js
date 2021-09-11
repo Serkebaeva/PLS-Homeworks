e812a53913f9e441ab1efdc9ef7efe6c79c829e6
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-09-11 16:18:24 +0600

    JS Lesson 2 - Variables, Constants - Basics (L. 26)

---
+++ lesson-26 JS constants and variables/js/main.js
@@ -0,0 +1,14 @@
+/*let userName = "Ivan";
+let adminName;
+adminName = userName;
+alert(adminName);*/
+
+let a = 100;
+let b = 200;
+let temp = a;
+
+a = b;
+b = temp;
+
+alert("Значение a = " + a);
+alert("Значение b = " + b);
