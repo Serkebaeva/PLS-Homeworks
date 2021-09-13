ceb719c54429eb1a31c6e6dac329198c482637cf
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-09-13 18:38:38 +0600

    Lesson 3 JS. 27

---
+++ lesson-27 JS Data Structures/js/main.js
@@ -0,0 +1,40 @@
+/*let myNumber = 10;
+myNumber = 10.5;
+myNumber = "Ivan";*/
+
+//Infinity (9/0)
+//-Infinity
+//NaN (not a number)
+/*BigInt --> 2^53 (900719925474099) norma/ 
+esli number bolshe -togda use BigInt 
+let MyBigNum=65464545545478787454545564n;
+let MyBigNum = 65464545545478787454545564n;*/
+
+//Strings
+/*let myString = "I don't know";
+let myString2 = 'I said "I am awesome"\' \\'; //ekranirovanie*/
+
+// template Strings - shablonnyie stroki
+/*let userName = "John";
+let message = `"Hello, ${userName}"`;*/
+
+//Boolean
+/*может принимать только 2 значения : false or true*/
+/*let user = "John";
+let userAge = 45;
+let isMarried = false;*/
+
+//Null (пустота)
+/*let age = null;
+age = 25;*/
+
+//undefined - значение не присвоено
+
+let myString = "Ivan";
+let myNumber = 500;
+let myNumber2 = 500.1;
+let MyBigNum = 65464545545478787454545564n;
+let age = null;
+let userName;
+
+console.log(typeof userName);
