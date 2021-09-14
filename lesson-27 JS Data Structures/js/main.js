bed0a7d956d80e69dea0e240de0e58406710281b
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-09-14 10:20:48 +0600

    JS. Lesson 3

--- lesson-27 JS Data Structures/js/main.js
+++ lesson-27 JS Data Structures/js/main.js
@@ -30,11 +30,55 @@ age = 25;*/
 
 //undefined - значение не присвоено
 
-let myString = "Ivan";
+/*let myString = "Ivan";
 let myNumber = 500;
 let myNumber2 = 500.1;
 let MyBigNum = 65464545545478787454545564n;
 let age = null;
-let userName;
+let userName;*/
 
-console.log(typeof userName);
+//+
+/*let a = 6;
+let b = 5;*/
+/*let result = a + b;*/
+
+//-
+/*let result = a - b;*/
+
+//*
+/*let result = a * b;*/
+
+// /
+/*let result = a / b;*/
+
+// %
+/*let result = a % b;*/
+
+// **
+/*console.log((2 + 2) * 5);*/
+
+// Increment
+/*let counter = 5;
+counter--;
+
+console.log(counter);*/
+
+//Object Math
+/*const pi = Math.PI;
+
+let a = Math.round(pi);
+let b = Math.ceil(pi);
+let c = Math.floor(pi);
+
+console.log(pi);
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log(pi.toFixed());*/
+
+//randomizer
+let a = Math.random();
+a = Math.floor(Math.random() * 20 + 1);
+
+console.log(a);
