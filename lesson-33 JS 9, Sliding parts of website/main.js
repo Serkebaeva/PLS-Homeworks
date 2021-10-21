1b4b45778a17e686ae36e1dbc4c9124a2532bfbc
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-10-21 14:15:36 +0600

    lesson-31, JS 7 & lesson-33, JS 8.

---
+++ lesson-33 JS 9, Sliding parts of website/main.js
@@ -0,0 +1,26 @@
+//INCREMENT - Порядок срабатывания
+/*let a = 1,
+  b = 1,
+  c,
+  d;
+c = ++a;
+console.log(c);
+d = b++;
+console.log(d);
+c = 2 + ++a;
+console.log(c);
+d = 2 + b++;
+console.log(d);
+console.log(a);
+console.log(b);*/
+
+//Zadacha
+let a = 2;
+let x = 1 + (a *= 2);
+console.log(x);
+
+// sravnenie strok
+console.log("d" < "c");
+console.log("b" < "d");
+console.log("k" < "a");
+console.log("b" > "a");
