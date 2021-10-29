a24d90d5ea250e748b11947a8e26f9d572d519d9
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2021-10-29 08:51:58 +0600

    Promt

---
+++ lesson-26 JS constants and variables/js/script.js
@@ -0,0 +1,11 @@
+/////////////////1-zadanie///////////////////////
+let name = "Aizada";
+let adress = "Bishkek, Kyrgyz Republic, Chuy avenue 219";
+
+console.log(name, adress);
+/////////////////////////////////////////////////
+
+/////////////////2-zadanie///////////////////////
+let userName = prompt("Как ваше Имя?");
+
+document.write('<h1>"Добро пожаловать, ' + userName + ' !" </h1>');
